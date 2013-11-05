4441581c4622a9c3e4cbbf9a69a205f2e0df44fe
Kat Hoffert <kat.hoffert@gmail.com>
2013-11-05 13:51:00 -0600

    Removed unnecessary, unused utility function.

--- src/conversions.js
+++ src/conversions.js
@@ -158,37 +158,6 @@ function tooBigWarning()
 
 
 /**
- * Utility function to ensure output is in fixed decimal form instead
- * of scientific notation.
- * Written by outis: http://stackoverflow.com/a/1685917/1968462
- * TODO: Contemplate if this is still needed
- */
-function toFixed(x)
-{
-	if(Math.abs(x) < 1.0)
-	{
-		var e = parseInt(x.toString().split('e-')[1]);
-		if(e)
-		{
-			x *= Math.pow(10,e-1);
-			x = '0.' + (new Array(e)).join('0') + x.toString().substring(2);
-		}
-	}
-	else
-	{
-		var e = parseInt(x.toString().split('+')[1]);
-		if(e > 20)
-		{
-			e -= 20;
-			x /= Math.pow(10, e);
-			x += (new Array(e + 1)).join('0');
-		}
-	}
-	return x;
-}
-
-
-/**
  * Simply utility class for converting unsigned binary to integers.
  */
 function binaryToDecimal(binary)
