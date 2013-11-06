304a3b9301df2afd9ff89c65575efaea79b3589a
Kat Hoffert <kat.hoffert@gmail.com>
2013-11-06 01:15:34 -0600

    Implemented show me buttons for other bases.
    All bases except decimal have a show me button, which will demonstrate
    the conversion to and from that base (to or from decimal) as well as
    summarize the base and its uses.
    
    Currently the show me buttons just create a largely empty box. The
    content to that will come shortly.

--- src/showMe.js
+++ src/showMe.js
@@ -19,6 +19,51 @@ function binaryFractionToDecimal(binary)
 	return decimal;
 }
 
+
+/**
+ * Creates the show me panel for demonstrating how the floating point representation.
+ */
+function baseShowMe(field, base, name)
+{
+	// Create the cover and popup as hidden, then fade it in
+	jQuery('<div/>', {
+		id: 'showMeCover',
+		style: 'display: none;',
+	}).appendTo('body');
+
+	jQuery('<div/>', {
+		id: 'showMePopup',
+		style: 'display: none;',
+	}).appendTo('#showMeCover');
+	$('#showMeCover, #showMePopup').fadeIn(500);
+
+	// Header
+	$('#showMePopup').append('<span id="showMeClose">X</span><h2>Show me: ' + name + '</h2>');
+
+	// Display base and value
+	$('#showMePopup').append('<p><b>Base:</b> ' + String(base) + '</p>');
+	$('#showMePopup').append('<p><b>Value:</b> <span class="binaryText">' + $(field).val() + '</span></p>');
+
+	// TODO: Implement intro paragraph summarizing the base and its uses
+	// Implement conversion to decimal
+	// Implement conversion from decimal
+
+	// Close when the background or close button is clicked
+	$('#showMeCover, #showMeClose').on('click', function(e){
+		// Only get clicks to the cover, not the popup
+		if(e.target !== this)
+		{
+			return;
+		}
+
+		// Fade the show me out and then remove it
+		$('#showMeCover, #showMePopup').fadeOut(500, function(){
+			$('#showMeCover, #showMePopup').remove();
+		});
+	});
+}
+
+
 /**
  * Creates the show me panel for demonstrating how the floating point representation.
  */
@@ -26,40 +71,40 @@ function floatShowMe(signField, exponentField, fractionField, exponentBits, frac
 {
 	// Create the cover and popup as hidden, then fade it in
 	jQuery('<div/>', {
-		id: 'floatShowMeCover',
+		id: 'showMeCover',
 		style: 'display: none;',
 	}).appendTo('body');
 
 	jQuery('<div/>', {
-		id: 'floatShowMePopup',
+		id: 'showMePopup',
 		style: 'display: none;',
-	}).appendTo('#floatShowMeCover');
-	$('#floatShowMeCover, #floatShowMePopup').fadeIn(500);
+	}).appendTo('#showMeCover');
+	$('#showMeCover, #showMePopup').fadeIn(500);
 
 	// Header
-	$('#floatShowMePopup').append('<span id="floatShowMeClose">X</span><h2>Show me: IEEE 754 floating point conversion</h2>');
+	$('#showMePopup').append('<span id="showMeClose">X</span><h2>Show me: IEEE 754 floating point conversion</h2>');
 
 	// Special case: Infinity
 	if(/^1+$/.test($(exponentField).val()) && /^0+$/.test($(fractionField).val()))
 	{
-		$('#floatShowMePopup').append('<p>The exponent field is as large as possible (all ones) while the fraction field is as small as possible (all zeros). This is the special case for infinity.</p>');
+		$('#showMePopup').append('<p>The exponent field is as large as possible (all ones) while the fraction field is as small as possible (all zeros). This is the special case for infinity.</p>');
 		if($(signField).val() == '1')
 		{
-			$('#floatShowMePopup').append('<p>Since the sign bit is <span class="binaryText">1</span>, this becomes negative infinity.</p>');
+			$('#showMePopup').append('<p>Since the sign bit is <span class="binaryText">1</span>, this becomes negative infinity.</p>');
 		}
 	}
 	// Special case: NaN
 	else if(/^1+$/.test($(exponentField).val()))
 	{
-		$('#floatShowMePopup').append('<p>The exponent field is as large as possible (all ones) while the fraction field is <b>not</b> as small as possible (all zeros). This is the special case for NaN (not a number). This is generally used to signal errors, such as division by zero or trying to do arithmetic on something that isn\'t a number. The sign bit is generally ignored. Sometimes the value of the fraction is used as a "signal" to tell the program exactly why the value is NaN.</p>');
+		$('#showMePopup').append('<p>The exponent field is as large as possible (all ones) while the fraction field is <b>not</b> as small as possible (all zeros). This is the special case for NaN (not a number). This is generally used to signal errors, such as division by zero or trying to do arithmetic on something that isn\'t a number. The sign bit is generally ignored. Sometimes the value of the fraction is used as a "signal" to tell the program exactly why the value is NaN.</p>');
 	}
 	// Special case: zero
 	else if(/^0+$/.test($(exponentField).val()) && /^0+$/.test($(fractionField).val()))
 	{
-		$('#floatShowMePopup').append('<p>The exponent and fraction fields are all zero, which make up the special case for zero.</p>');
+		$('#showMePopup').append('<p>The exponent and fraction fields are all zero, which make up the special case for zero.</p>');
 		if($(signField).val() == '1')
 		{
-			$('#floatShowMePopup').append('<p>Since the sign bit is <span class="binaryText">1</span>, this becomes negative zero. The IEEE 754 specification has two values for zero, positive and negative zero. It is up to software to separate these (they\'re usually treated as equal).</p>');
+			$('#showMePopup').append('<p>Since the sign bit is <span class="binaryText">1</span>, this becomes negative zero. The IEEE 754 specification has two values for zero, positive and negative zero. It is up to software to separate these (they\'re usually treated as equal).</p>');
 		}
 	}
 	else
@@ -69,12 +114,12 @@ function floatShowMe(signField, exponentField, fractionField, exponentBits, frac
 		if($(signField).val() == '1')
 		{
 			negative = true;
-			$('#floatShowMePopup').append('<p><b>Sign bit:</b> <span class="binaryText">' + $(signField).val() + '</span> = Negative</p>');
+			$('#showMePopup').append('<p><b>Sign bit:</b> <span class="binaryText">' + $(signField).val() + '</span> = Negative</p>');
 		}
 		else
 		{
 			negative = false;
-			$('#floatShowMePopup').append('<p><b>Sign bit:</b> <span class="binaryText">' + $(signField).val() + '</span> = Positive</p>');
+			$('#showMePopup').append('<p><b>Sign bit:</b> <span class="binaryText">' + $(signField).val() + '</span> = Positive</p>');
 		}
 
 		// Exponent
@@ -82,11 +127,11 @@ function floatShowMe(signField, exponentField, fractionField, exponentBits, frac
 		// Not a subnormal
 		if(!/^0+$/.test($(exponentField).val()))
 		{
-			$('#floatShowMePopup').append('<p><b>Exponent:</b> <span class="binaryText">' + $(exponentField).val() + '</span> = ' + String(binaryToDecimal($(exponentField).val())) + '; subtract the bias (' + bias + ') to get ' + String(exponent) + '</p>');
+			$('#showMePopup').append('<p><b>Exponent:</b> <span class="binaryText">' + $(exponentField).val() + '</span> = ' + String(binaryToDecimal($(exponentField).val())) + '; subtract the bias (' + bias + ') to get ' + String(exponent) + '</p>');
 		}
 		else
 		{
-			$('#floatShowMePopup').append('<p><b>Exponent:</b> <span class="binaryText">' + $(exponentField).val() + '</span> = ' + String(binaryToDecimal($(exponentField).val())) + '; This is the special case of a subnormal (exponent of zero and non-zero fraction)</p>');
+			$('#showMePopup').append('<p><b>Exponent:</b> <span class="binaryText">' + $(exponentField).val() + '</span> = ' + String(binaryToDecimal($(exponentField).val())) + '; This is the special case of a subnormal (exponent of zero and non-zero fraction)</p>');
 		}
 
 		// Fraction
@@ -96,18 +141,18 @@ function floatShowMe(signField, exponentField, fractionField, exponentBits, frac
 			// Subnormal
 			binaryFraction = '0.' + $(fractionField).val();
 			exponent = -bias + 1;
-			$('#floatShowMePopup').append('<p><b>Fraction:</b> <span class="binaryText">' + $(fractionField).val() + '</span>; this is a subnormal, so no leading <span class="binaryText">1</span> is added, leaving us with <span class="binaryText">' + binaryFraction + '</span></p><p>In addition, subnormals always have an exponent of ' + String(exponent) + '. This is because normally the smallest possible exponent is 2<sup>' + String(exponent) + '</sup>. In such a case, however, there is always a leading one. Since subnormals do not have a leading one, the exponent remains at ' + String(exponent) + ' and we get smaller numbers by inserting more zeroes after the decimal. This lets us go beyond the ' + String(exponentBits) + ' bits offered by the exponent field.</p>');
+			$('#showMePopup').append('<p><b>Fraction:</b> <span class="binaryText">' + $(fractionField).val() + '</span>; this is a subnormal, so no leading <span class="binaryText">1</span> is added, leaving us with <span class="binaryText">' + binaryFraction + '</span></p><p>In addition, subnormals always have an exponent of ' + String(exponent) + '. This is because normally the smallest possible exponent is 2<sup>' + String(exponent) + '</sup>. In such a case, however, there is always a leading one. Since subnormals do not have a leading one, the exponent remains at ' + String(exponent) + ' and we get smaller numbers by inserting more zeroes after the decimal. This lets us go beyond the ' + String(exponentBits) + ' bits offered by the exponent field.</p>');
 		}
 		else
 		{
 			// Normal
 			binaryFraction = '1.' + $(fractionField).val();
-			$('#floatShowMePopup').append('<p><b>Fraction:</b> <span class="binaryText">' + $(fractionField).val() + '</span>; add the implied leading <span class="binaryText">1</span> to get <span class="binaryText">' + binaryFraction + '</span></p>');
+			$('#showMePopup').append('<p><b>Fraction:</b> <span class="binaryText">' + $(fractionField).val() + '</span>; add the implied leading <span class="binaryText">1</span> to get <span class="binaryText">' + binaryFraction + '</span></p>');
 		}
 
 		// Put it all together
 		var fractionDecimal = String(binaryFractionToDecimal(binaryFraction));
-		$('#floatShowMePopup').append('<p>In decimal, the fraction component is ' + fractionDecimal + '.</p>');
+		$('#showMePopup').append('<p>In decimal, the fraction component is ' + fractionDecimal + '.</p>');
 		if(negative)
 		{
 			var actualValue = -1 * fractionDecimal * Math.pow(2, exponent);
@@ -116,7 +161,7 @@ function floatShowMe(signField, exponentField, fractionField, exponentBits, frac
 			{
 				equalsSign = '= ';
 			}
-			$('#floatShowMePopup').append('<p>Since the sign bit is negtative, this number will also be negative, causing us to end up with -1 &times; ' + fractionDecimal + ' &times; 2<sup>' + exponent + '</sup> ' + equalsSign + actualValue + '.</p>');
+			$('#showMePopup').append('<p>Since the sign bit is negtative, this number will also be negative, causing us to end up with -1 &times; ' + fractionDecimal + ' &times; 2<sup>' + exponent + '</sup> ' + equalsSign + actualValue + '.</p>');
 		}
 		else
 		{
@@ -126,12 +171,12 @@ function floatShowMe(signField, exponentField, fractionField, exponentBits, frac
 			{
 				equalsSign = '= ';
 			}
-			$('#floatShowMePopup').append('<p>This causes us to end up with ' + fractionDecimal + ' &times; 2<sup>' + exponent + '</sup> ' + equalsSign + actualValue + '.</p>');
+			$('#showMePopup').append('<p>This causes us to end up with ' + fractionDecimal + ' &times; 2<sup>' + exponent + '</sup> ' + equalsSign + actualValue + '.</p>');
 		}
 	}
 
 	// Close when the background or close button is clicked
-	$('#floatShowMeCover, #floatShowMeClose').on('click', function(e){
+	$('#showMeCover, #showMeClose').on('click', function(e){
 		// Only get clicks to the cover, not the popup
 		if(e.target !== this)
 		{
@@ -139,8 +184,8 @@ function floatShowMe(signField, exponentField, fractionField, exponentBits, frac
 		}
 
 		// Fade the show me out and then remove it
-		$('#floatShowMeCover, #floatShowMePopup').fadeOut(500, function(){
-			$('#floatShowMeCover, #floatShowMePopup').remove();
+		$('#showMeCover, #showMePopup').fadeOut(500, function(){
+			$('#showMeCover, #showMePopup').remove();
 		});
 	});
 }
@@ -165,5 +210,60 @@ $('#doubleShowMe').bind("click", function(){
 	else
 	{
 		blinkField('#doubleSign, #doubleExponent, #doubleFraction');
+	}
+});
+
+$('#binShowMe').bind("click", function(){
+	if(/^-?[01]+\.?[01]*$/i.test($('#bin').val()))
+	{
+		baseShowMe('#bin', 2, 'Binary');
+	}
+	else
+	{
+		blinkField('#bin');
+	}
+});
+
+$('#octShowMe').bind("click", function(){
+	if(/^-?[0-7]+\.?[0-7]*$/i.test($('#bin').val()))
+	{
+		baseShowMe('#oct', 8, 'Octal');
+	}
+	else
+	{
+		blinkField('#oct');
+	}
+});
+
+$('#duoShowMe').bind("click", function(){
+	if(/^-?[0-9ABab]+\.?[0-9ABab]*$/i.test($('#duo').val()))
+	{
+		baseShowMe('#duo', 12, 'Duodecimal');
+	}
+	else
+	{
+		blinkField('#duo');
+	}
+});
+
+$('#hexShowMe').bind("click", function(){
+	if(/^-?[0-9A-Fa-f]+\.?[0-9A-Fa-f]*$/i.test($('#hex').val()))
+	{
+		baseShowMe('#hex', 16, 'Hexadecimal');
+	}
+	else
+	{
+		blinkField('#hex');
+	}
+});
+
+$('#b64ShowMe').bind("click", function(){
+	if(/^-?[0-9A-Za-z\+\/]+\.?[0-9A-Za-z\+\/]*$/i.test($('#b64').val()))
+	{
+		baseShowMe('#b64', 64, 'Base 64');
+	}
+	else
+	{
+		blinkField('#b64');
 	}
 });
